bb28a71825e0b17a2642511ef59cca40185b9c29
chaewoo3386 <kimcheawoo6885@gmail.com>
2025-05-07 17:41:36 +0900

    Add files via upload

---
+++ hw250507/MyDate.java
@@ -0,0 +1,9 @@
+package hw250507;
+
+public class MyDate {
+	int year = 2035;
+	int month = 12;
+	int day = 25;
+	
+
+}
---
+++ hw250507/NullPointerExceptionTest.java
@@ -0,0 +1,13 @@
+package hw250507;
+
+public class NullPointerExceptionTest {
+
+	public static void main(String[] args) {
+		MyDate d = new MyDate();
+		
+		System.out.printf("%d년 %d월 %d일\n", d.year, d.month, d.day);
+		
+
+	}
+
+}
